f10d50a552c01d2540377ece54acd6eb6b10c099
unknown <imed.siala@gmail.com>
2024-04-13 10:41:37 +0100

    first commiserveuddedddrdddd

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ dbConnect();
 app.use(morgan("dev"))
 app.use(cors())
 
-app.use(express.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser())
 app.use("/api/user", authRouter)
 app.use("/api/product", productRouter)
